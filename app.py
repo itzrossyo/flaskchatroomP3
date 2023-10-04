f1690b65603abf7119da8c2f8fa95a5aa6fbfc5a
itzrossyo <Rossespley@gmail.com>
2023-10-04 03:26:01 +0100

    added user logs and session and randiom codes for unique rooms

--- app.py
+++ app.py
@@ -8,11 +8,24 @@ app.config['SECRET_KEY'] = "f239f2aksf2fd£$@!@£@!$F4"
 socketio = SocketIO(app)
 
 rooms = {}
+
+
+# Function to generate a unique code
 def generate_unique_code(length):
+    while True:
+        code = ""
+        for _ in range(length):
+            code += random.choice(ascii_uppercase)
+
+        if code not in rooms:
+            break
+
+        return code
 
 
 @app.route("/", methods=["POST", "GET"])
 def home():
+    session.clear()
     if request.method == "POST":
         name = request.form.get("name")
         code = request.form.get("code")
@@ -20,14 +33,22 @@ def home():
         create = request.form.get("create", False)
 
         if not name:
-            return render_template("home.html", error="Please enter a name.")
+            return render_template("home.html", error="Please enter a name.", code=code, name=name)
 
-        if join != False and not code:
-            return render_template("home.html", error="please enter a code.")
+        if join and not code:
+            return render_template("home.html", error="Please enter a code.", code=code, name=name)
 
-        room = code
-        if create != False:
-            room = generate_unique_code
+    room = code
+    if create != False:
+        room = generate_unique_code(4)
+        rooms[room] = {"members": 0, "messages": []}
+    elif code not in rooms:
+        return render_template("home.html", error="Room dose not exist.", code=code, name=name)
+
+    session["room"] = room
+    session["name"] = name
+    # Return a response or render a template
+    return render_template("home.html")
 
 
 if __name__ == '__main__':
